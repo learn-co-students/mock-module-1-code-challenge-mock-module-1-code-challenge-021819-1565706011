d8235de500a108d45f6a9e2e65808750f9bff342
resham <resham.arora@gmail.com>
2019-08-13 10:42:46 -0500

    your message

--- book.rb
+++ book.rb
@@ -1,1 +1,19 @@
+class Book
 
+    attr_accessor :author :title :word_count
+
+end
+
+book1 = book.new("Author1", "Title1", "1364")
+book2 = book.new("Author2", "Title2", "1634")
+
+
+def book.all
+    array = []
+    array.select {|book| true}
+
+end
+puts array
+end
+
+def book_title
