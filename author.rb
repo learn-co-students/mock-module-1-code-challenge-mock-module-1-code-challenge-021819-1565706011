f3e6eac877fa60a39ef0fadac0f90da8a7b4d7b3
Chase Smith <chases832@gmail.com>
2019-08-13 10:34:16 -0500

    cool first try

--- author.rb
+++ author.rb
@@ -1,1 +1,47 @@
+class Author
+    def initialize(author,author_books,word_count)
+        @author = author
+        @author_books = author_books
+        @word_count = word_count
 
+    end 
+    @author = []
+    @author_books = []
+    @word_count = []
+
+
+    def author_pusher
+        @author << author.new
+    end 
+    def author_book_pusher
+        @author_books << author_books.new 
+    end
+    def author_word_count_pusher
+        @word_count << word_count.new
+    end 
+    def author.all
+        @author 
+    end 
+    # def author_book
+    #     @author_books 
+    # end 
+    def author_book
+        author_books.each do |book|
+            if @author_books = self
+                puts @author_books
+            end
+        end 
+    end 
+    def  write_book(title, word_count)
+        book.book_author.new(title, word_count)
+    end
+    def total_words
+
+        self.book.all.book_word_count
+    end 
+    def most_words
+        word_count.max
+        return word_count.max
+
+    end 
+end 
--- book.rb
+++ book.rb
@@ -1,1 +1,26 @@
+class Book
+    @books = []
+    def initialize(book_title, book_author, book_word_count)
+        @book_title = book_title
+        @book_author = book_author
+        @book_word_count = book_word_count 
+    end 
+    def book_pusher
+        @books << book.new
+    end 
+    
+    def book.all
+        @books 
+    end 
+    def book_autor
+        self.book_author
+    end 
+    def book_title
+        self.book_title
+    end 
+    def book_word_count
+        self.book_word_count
+    end 
 
+
+end 
