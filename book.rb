4eb01a07038d22a382c365574e612654fcf1deea
Your Name <you@example.com>
2019-08-13 10:33:18 -0500

    Done.

--- book.rb
+++ book.rb
@@ -1,1 +1,16 @@
+class Book
 
+    attr_accessor :title, :author, :word_count
+
+    def initialize(title, author, word_count)
+        @title = title 
+        @author = author
+        @word_count = word_count 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end
+
+end
--- console.rb
+++ console.rb
@@ -2,5 +2,4 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 require_relative './book_author.rb'
-
 binding.pry
