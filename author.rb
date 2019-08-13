6ac663fbc7a50f5710f35d95111a8b836d9fe7f6
chandonporter <cneilporter94@gmail.com>
2019-08-13 10:34:35 -0500

    done

--- author.rb
+++ author.rb
@@ -1,1 +1,25 @@
+class Author
+
+    @@all = []
+
+    attr_accessor :titles, :books
+
+    def initialize(titles, books)
+        @titles = titles
+        @books = books
+        @@all << self
+    end
+
+    def author_book
+        @books
+    end
+
+    def write_book(title, word_count)
+
+    def self.all
+        @@all
+    end
+
+
+end
 
--- book_author.rb
+++ book_author.rb
@@ -1,1 +1,37 @@
+class Book_author
+
+    @@all = []
+
+    attr_accessor :author, :book
+
+    def initialize(author, book, author_name, book_name)
+        @author = author
+        @book = book
+        @author_name = author_name
+        @book_name = book_name
+        @@all << self
+    end
+
+    def author
+        self.author
+    end
+
+    def book
+        self.book
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def authors
+        @author_name
+    end
+
+    def books
+        @book_name
+    end
+
+
+end
 
