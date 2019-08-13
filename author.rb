0bdfd69c3ee71aeedb8c8a5363eb80866993e42e
Emily <mle.jo.11@gmail.com>
2019-08-13 10:47:25 -0500

    working on relationships

--- author.rb
+++ author.rb
@@ -1,1 +1,38 @@
+class Author
+@@all = []
 
+attr_accessor :books
+
+def initialize
+@@all << self
+end
+
+def self.all
+    @@all
+end
+
+def write_book(title, word_count)
+    
+    book.author = self
+end
+
+def total_words
+
+end
+
+def most_words
+
+end
+
+end
+
+#- `Author.all`
+#should return all of the authors
+# `Author#books`
+#should return all of the books an author has written
+#- `Author#write_book`
+#should take arguments of a title and word count and make a new Book instance associated with this author
+#- `Author#total_words`
+#should return the total number of words that author has written across all of their authored books.
+#- `Author.most_words`
+#should return the author instance who has written the most words
--- book.rb
+++ book.rb
@@ -1,1 +1,25 @@
+class Book
+@@all = []
+attr_accessor :title, :author, :word_count
 
+def initialize(title, author, word_count)
+    @title = title
+    @author = author
+    @word_count = word_count
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+end
+
+#- `Book.all`
+#should return all of the books
+#- `Book#author`
+#should return the author instance who wrote this book
+#- `Book#title`
+#should return the title of the book
+#- `Book#word_count`
+#should return the number of words in the book
